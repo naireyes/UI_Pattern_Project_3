b35f2dc54495a0d8e9de2811c2c7dba45bbdbdea
Naira_Reyes <gab1ana1@gmail.com>
2020-07-04 19:44:30 -0400

    Positioned images

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // const apiKey = 'vL6U1HWoZmZcIF0ETK84r2Lny3ObVHhtuGUpMN_7YgI'
-const url = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl';
+const url = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
 
 fetch(url)
     .then(res => res.json())
@@ -14,29 +14,24 @@ fetch(url)
     })
 
 
-// let i;
+let slideIndex = 1;
+showImgs(slideIndex);
 
-// let slides = document.querySelector("chow-image");
+function plusDivs(n) {
+    showImgs(slideIndex += n);
+}
 
-// let dots = document.querySelector("dot");
-
-// for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-// }
-
-// slideIndex++;
-
-// if (slideIndex > slides.length) {
-//     slideIndex = 1;
-// }
-
-// for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.
-//     replace(" active", "");
-// }
-
-// slides[slideIndex - 1].style.display = "block";
-// dots[slideIndex - 1].className += " active";
-
-// // Change image every 2 seconds 
-// setTimeout(showSlides, 2000);  
+function showImgs(n) {
+    let i;
+    let x = document.getElementsByClassName("maybelline");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
